0408e2a7fe98ebbda8295108ce5ce03d734fd308
dqwdd <dqwdd@naver.com>
2021-11-17 01:58:35 +0900

    1. button_background_green.xml 생성 2. 레이아웃에서 = 기호 버튼 백그라운드 변경 3. 메인 코드 작성중

--- app/src/main/java/com/fastcampus/calculater/MainActivity.kt
+++ app/src/main/java/com/fastcampus/calculater/MainActivity.kt
@@ -3,26 +3,65 @@ package com.fastcampus.calculater
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    private val expressionTextView: TextView by lazy {
+        findViewById<TextView>(R.id.expressionTextView)
+    }
+
+    private val resultTextView: TextView by lazy {
+        findViewById<TextView>(R.id.resultTextView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun buttonClicked(v:View) {
+
+    //누르면 string값을 화면에 보여주는 함수
+    fun buttonClicked(v: View) {
+        when (v.id) {
+            R.id.button0 -> numberButtonClicked("0")
+            R.id.button1 -> numberButtonClicked("1")
+            R.id.button2 -> numberButtonClicked("2")
+            R.id.button3 -> numberButtonClicked("3")
+            R.id.button4 -> numberButtonClicked("4")
+            R.id.button5 -> numberButtonClicked("5")
+            R.id.button6 -> numberButtonClicked("6")
+            R.id.button7 -> numberButtonClicked("7")
+            R.id.button8 -> numberButtonClicked("8")
+            R.id.button9 -> numberButtonClicked("9")
+
+            R.id.buttonPlus -> operatorButtonClicked("+")
+            R.id.buttonMinus -> operatorButtonClicked("-")
+            R.id.buttonMulti -> operatorButtonClicked("*")
+            R.id.buttonDivider -> operatorButtonClicked("/")
+            R.id.buttonModulo -> operatorButtonClicked("%")
+        }
+    }
+
+    private fun numberButtonClicked(number: String) {
         //
     }
 
-    fun resultButtonClicked(v:View) {
+    private fun operatorButtonClicked(operator: String) {
         //
     }
 
-    fun historyButtonClicked(v:View) {
+
+    fun resultButtonClicked(v: View) {
         //
     }
 
-    fun clearButtonClicked(v:View) {
+    fun historyButtonClicked(v: View) {
+        //
+    }
+
+    fun clearButtonClicked(v: View) {
         //
     }
 }
