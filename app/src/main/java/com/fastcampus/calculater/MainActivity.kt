9b895a0510ed08c84d43afa6893b98343dd2e593
dqwdd <dqwdd@naver.com>
2021-11-17 02:39:15 +0900

    1. button_background_green.xml 생성 2. 레이아웃에서 = 기호 버튼 백그라운드 변경 3. 메인 코드 작성중(Operator가 작성되었을 때의 예외 처리랑 색 입히는 것 까지 함

--- app/src/main/java/com/fastcampus/calculater/MainActivity.kt
+++ app/src/main/java/com/fastcampus/calculater/MainActivity.kt
@@ -2,11 +2,19 @@ package com.fastcampus.calculater
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    //계산기 조건
+    //1. 연산자는 두 번 누를 수 없고
+    //2. 숫자는 최대 15자리까지만
 
     private val expressionTextView: TextView by lazy {
         findViewById<TextView>(R.id.expressionTextView)
@@ -21,6 +29,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    private var isOperator = false
+    private var hasOperator = false
 
     //누르면 string값을 화면에 보여주는 함수
     fun buttonClicked(v: View) {
@@ -45,11 +55,70 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun numberButtonClicked(number: String) {
-        //
+        //조건
+        //숫자는 수자끼리 뭉쳐놓고 연산자를 쓸 경우는 한 칸 띄고 연산자 한 칸 띄고 숫자 이렇게 배치 할거임
+        //split으로 구분할건데 스페이스로 구분 하도록 할 예정임
+
+        if (isOperator) { //연산자를 클릭 후 들어왔을 경우 띄어쓰기 함
+            expressionTextView.append(" ")
+        }
+
+        isOperator = false
+
+        val expressionText = expressionTextView.text.split(" ")
+
+        if (expressionText.isNotEmpty() && expressionText.last().length >= 15) {
+            Toast.makeText(this, "15자리 까지만 사용할 수 있습니다.", Toast.LENGTH_SHORT).show()
+            return
+        } else if (expressionText.last().isEmpty() && number == "0") {
+            Toast.makeText(this, "0은 제일 앞에 올 수 없습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        expressionTextView.append(number)
+
+        //Todo resultTextView에 실시간으로 계산 결과를 넣어야 하는 기능
+
     }
 
     private fun operatorButtonClicked(operator: String) {
         //
+
+        if(expressionTextView.text.isEmpty()) {//빈 칸일 경우 안눌리게
+            return
+        }
+
+        when {
+            isOperator -> { //연산자를 눌렀는데 다른 연산자를 바꾸고 싶은 경우
+                //전에 눌렀던 연산자를 지우고(drop) 새로 씀
+                val text = expressionTextView.text.toString()
+                expressionTextView.text = text.dropLast(1) + operator//맨 끝에서부터 1자리만 지워줌
+            }
+
+            hasOperator -> { //연산자를 누르고 다시 연산자를 눌렀을 경우
+                Toast.makeText(this, "연산자는 한 번만 사용할 수 있습니다.", Toast.LENGTH_SHORT).show()
+                return
+            }
+
+            else -> {
+                expressionTextView.append(" ${operator}")
+            }
+
+        }
+
+        //연산자 색 바꾸는 코드
+        val ssb = SpannableStringBuilder(expressionTextView.text)
+        ssb.setSpan(
+            ForegroundColorSpan(resources.getColor(R.color.green)),
+            expressionTextView.text.length -1,
+            expressionTextView.text.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        expressionTextView.text = ssb
+
+        isOperator = true
+        hasOperator = true
+
     }
 
 
